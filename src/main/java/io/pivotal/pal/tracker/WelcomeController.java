7ed67da63e999dd5acbbfa7fa223c461798bfe99
sohamsachin <shmahajan23@gmail.com>
2019-06-25 12:27:52 +0530

    [PCF]Configure app with Manifest.yml

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,12 +2,19 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class WelcomeController {
 
+    String helloMessage;
+
+    public WelcomeController( @Value("${welcome.message}") String helloMessage) {
+        this.helloMessage = helloMessage;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return helloMessage;
     }
 }
